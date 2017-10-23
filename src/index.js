759a5f32a2e70842e7cbb52f2b45f8e89b36bf8f
Stephen Bellchester <stephen.bellchester@auspost.com.au>
2017-10-23 13:27:10 +1100

    Switched to javascript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import * as request from 'request';
-
-export function simpleHttpRequest (hook:any) {
+module['exports'] = function simpleHttpRequest (hook) {
     // npm modules available, see: http://hook.io/modules
+    var request = require('request');
     request.get('http://httpbin.org/ip', function(err, res, body){
         if (err) {
             return hook.res.end(err.messsage);
